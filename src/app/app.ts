9a22ffb78f359852fffd3eb4360e5dd28e4a1915
Joel <99019728+joelderG@users.noreply.github.com>
2026-03-04 23:49:16 +0100

    added general behavior & added components to app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,25 @@
 import { Component, HostListener, inject, OnInit, signal } from '@angular/core';
-import { SliceHeatmapComponent } from "@components/slice-heatmap/slice-heatmap.component";
+import { CuttingPlaneComponent } from "@components/cutting-plane/cutting-plane.component";
 import { VolumeViewerComponent } from '@components/volume-viewer/volume-viewer.component';
 import { ApiService } from '@services/api-service/api.service';
+import { AppStateService } from '@services/app-state-service/app-state.service';
 
 @Component({
   selector: 'app-root',
-  imports: [SliceHeatmapComponent, VolumeViewerComponent],
+  imports: [CuttingPlaneComponent, VolumeViewerComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
   protected readonly title = signal('reflex-geo-explore');
   private readonly apiService = inject(ApiService);
+  private readonly appStateService = inject(AppStateService);
 
   protected zIndex = 0;
   protected xCoords: number[] = [];
   protected yCoords: number[] = [];
   protected zCoords: number[] = [];
   protected classes: number[] = [];
-  protected classVisible: boolean[] = [];
 
   ngOnInit() {
     this.apiService.getMeta().subscribe((metaData) => {
@@ -27,7 +28,7 @@ export class App implements OnInit {
       this.zCoords = metaData.z_coords;
       this.classes = metaData.classes;
       this.zIndex = Math.floor(metaData.z_coords.length / 2);
-      this.classVisible = new Array(metaData.classes.length).fill(true);
+      this.appStateService.initializeClasses(metaData.classes);
     });
   }
 
@@ -39,11 +40,10 @@ export class App implements OnInit {
       this.zIndex = Math.max(this.zIndex - 1, 0);
     if (e.key >= '0' && e.key <= '9') {
       const i = parseInt(e.key);
-      if (i < this.classVisible.length) {
-        const updated = [...this.classVisible];
-        updated[i] = !updated[i];
-        this.classVisible = updated;
-      }
+      this.appStateService.toggleClassVisibilityAtIndex(i);
+    }
+    if (e.key === 't' || e.key === 'T') {
+      this.appStateService.toggleShowOnlyCurrentSlicePoints();
     }
   }
 }
