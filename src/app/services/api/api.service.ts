3b7159a9c8a42dfffb46b4b5561472aa65a52639
Joel <99019728+joelderG@users.noreply.github.com>
2026-03-28 17:02:39 +0100

    only one volume http request

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { MetaData, Volume } from './api.types';
+import { shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,13 @@ import { MetaData, Volume } from './api.types';
 export class ApiService {
   private readonly http = inject(HttpClient);
   private root = "http://127.0.0.1:8000";
+  private readonly volume$ = this.http.get<Volume>(`${this.root}/volume`).pipe(shareReplay(1));
 
   getMeta() {
     return this.http.get<MetaData>(`${this.root}/meta`);
   }
 
   getVolume() {
-    return this.http.get<Volume>(`${this.root}/volume`);
+    return this.volume$;
   }
 }
